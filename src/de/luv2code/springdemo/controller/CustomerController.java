b653320ee40488581a031dbd7951f5f1f3967a78
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-08-13 16:08:33 +0200

    add CustomerService add CustomerServiceImpl

--- src/de/luv2code/springdemo/controller/CustomerController.java
+++ src/de/luv2code/springdemo/controller/CustomerController.java
@@ -8,26 +8,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import de.luv2code.springdemo.dao.CustomerDAO;
 import de.luv2code.springdemo.entity.Customer;
+import de.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	// need to inject the customer dao
+	// need to inject our customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@GetMapping("/list")
 	public String listCostumers(Model theModel) {
 		
-		// get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
-		
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		// add the customers to the model 
 		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-costumers";
 	}
+	
+	
 }
--- src/de/luv2code/springdemo/service/CustomerService.java
+++ src/de/luv2code/springdemo/service/CustomerService.java
@@ -1,5 +1,10 @@
 package de.luv2code.springdemo.service;
 
+import java.util.List;
+
+import de.luv2code.springdemo.entity.Customer;
+
 public interface CustomerService {
 
+	public List<Customer> getCustomers();
 }
